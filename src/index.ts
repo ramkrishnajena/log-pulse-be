81d023aa48a10f5f6ce8f14a73a1c16cfb6a8539
krushna377 <krushna377@gmail.com>
2026-02-06 22:17:40 +0530

    created controller for logs route

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import logRoutes from "./routes/log.routes.js";
 dotenv.config();
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use("/api/v1", logRoutes);
 app.get("/test", (req, res): void => {
   res.json({ test: "Hello, this is a test endpoint!" });
 });
